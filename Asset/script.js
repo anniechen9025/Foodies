f15a85ebc02a36aca81254a7b3d476885f75a696
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-03-17 19:18:51 -0700

    clean the code

--- Asset/script.js
+++ Asset/script.js
@@ -1,60 +1,12 @@
-// var key = "AIzaSyAeWrxgOM61l2hJVTiqUpKdmf2pw_E_qa4";
-// var corsUrl = "https://cors-anywhere.herokuapp.com/";
-// var requestUrl = corsUrl + "https://www.googleapis.com/geolocation/v1/geolocate?key=" + key;
-// var body = {
-//     "homeMobileCountryCode": 310,
-//     "homeMobileNetworkCode": 410,
-//     "radioType": "gsm",
-//     "carrier": "Vodafone",
-//     "considerIp": "true",
-//     "cellTowers": [
-//         {
-//             "cellId": 170402199,
-//             "locationAreaCode": 35632,
-//             "mobileCountryCode": 310,
-//             "mobileNetworkCode": 410,
-//             "age": 0,
-//             "signalStrength": -60,
-//             "timingAdvance": 15
-//         }
-//     ],
-//     "wifiAccessPoints": [
-//         {
-//             "macAddress": "84:d4:7e:09:a5:f1",
-//             "signalStrength": -43,
-//             "age": 0,
-//             "channel": 11,
-//             "signalToNoiseRatio": 0
-//         }
-//     ],
-
-// };
-
-// function test() {
-//     fetch(requestUrl, {
-//         method: "POST",
-//         body: JSON.stringify(body)
-//     }).then(function (response) {
-//         return response.json();
-//     }).then(function (data) {
-//         console.log(data);
-//     });
-// };
-
-// test();
-
-// navigator.geolocation.getCurrentPosition(successCallback,
-//     errorCallback,
-//     {maximumAge:600000});
-// function successCallback(position) {
-// // By using the 'maximumAge' option above, the position
-// // object is guaranteed to be at most 10 minutes old.
-// console.log(position);
-// }
-// function errorCallback(error) {
-// // Update a div element with error.message.
-// console.log(error);
-// };
+navigator.geolocation.getCurrentPosition(successCallback,
+    errorCallback,
+    {maximumAge:600000});
+function successCallback(position) {
+console.log(position);
+}
+function errorCallback(error) {
+console.log(error);
+};
 
 let map;
 
