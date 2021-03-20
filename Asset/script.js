3fc19d5606ff4919e5eca8898f15af83b51c1cdd
Daniel Phan <dannyphan@comcast.net>
2021-03-19 23:23:10 -0700

    Added lat and long parameters to Yelp API

--- Asset/script.js
+++ Asset/script.js
@@ -3,19 +3,23 @@ const apiKey = "QUZUkG7TUxmpU7KluE2jyqZtI8PEZCZwXyyJWeZUrhk7LOhxoiDv1YaTYF9bNzVU
 // If error occurs, go to the website in console and request temporary access
 const corsUrl = "https://cors-anywhere.herokuapp.com/"
 const token = "Bearer QUZUkG7TUxmpU7KluE2jyqZtI8PEZCZwXyyJWeZUrhk7LOhxoiDv1YaTYF9bNzVU808diTb1CPGXeZKVmar4QRlMMgKxNvg5l_NzkPq40EXG7VF4kNioPiPhZTZQYHYx"
+let map;
 
 $(".searchBtn").on("click", function () {
-    let query = $("#userInput").val()
+    let userInput = $("#userInput").val()
     // If there is an empty value, return
-    var test = navigator.geolocation.getCurrentPosition() 
-    console.log(test)
-
-    // Passing a geolocation to a on click function jquery
-    if (query === "") {
+    if (userInput === "") {
         return;
     }
-    // query is taking the users input 
-    let yelpApi = `https://api.yelp.com/v3/businesses/search?term=${query}&location=98133`;
+    
+    let lngData = map.center.lng();
+    console.log(lngData)
+    
+    let latData = map.center.lat();
+    console.log(latData);
+
+    // userInput is taking the users input and using it as a parameter
+    let yelpApi = `https://api.yelp.com/v3/businesses/search?term=${userInput}&latitude=${latData}&longitude=${lngData}`;
     console.log(yelpApi)
 
     $.ajax({
@@ -27,6 +31,10 @@ $(".searchBtn").on("click", function () {
         }
     }).then(function (response) {
         console.log(response);
+        for (i = 0; i < response.businesses.length; i++) {
+          let yelpList = response.businesses[i]
+          console.log(yelpList);
+        }
     })
 });
 
@@ -41,7 +49,6 @@ function errorCallback(error) {
 console.log(error);
 };
 
-let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
